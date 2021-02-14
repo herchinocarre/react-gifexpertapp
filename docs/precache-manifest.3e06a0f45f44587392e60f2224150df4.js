f15698762d8727d7b4769519fa18e7cf315e960c
Hernan Carreno <herchinocarre@gmail.com>
2021-02-14 15:54:25 -0300

    docs agregado

---
+++ docs/precache-manifest.3e06a0f45f44587392e60f2224150df4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d4ecbc57c5f68b2ed4925c6c096ffc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f444e26c47e7a07fbf69",
+    "url": "/static/css/main.7e81ae07.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f444e26c47e7a07fbf69",
+    "url": "/static/js/main.53e4d6f5.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
